0f89c197df3a67800156a337de55a3f81f83687b
Thiago Lima <58890881+zTico@users.noreply.github.com>
2020-12-23 19:45:45 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" type="text/css" href="estilo.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="index.js"></script>
-    <title>Consumi  API - CEP</title>
+    <title>Consumo  API - CEP</title>
 </head>
 <body>
 
